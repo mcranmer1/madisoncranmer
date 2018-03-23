cfe45f9395f10d1917ffd9bfb89a6dbab8321b2f
mcranmer1 <35708880+mcranmer1@users.noreply.github.com>
2018-03-22 22:38:33 -0700

    Version 1.3

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,13 @@
 console.log('Be like Browser.');
 
-// Light Gallery
-$('#gallery-1').lightGallery();
+// Randomizer
+var max = 2;
+var randNum = Math.floor( Math.random() * max );
 
+// Store URL
+var img0 = '../img/professionalpic.jpg';
+var img1 = '../img/madimountain.jpg';
+
+// Change Image
+$('#replace').attr( 'src', eval('img' + randNum) );
 
